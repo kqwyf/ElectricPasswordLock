6adb0856d81fff3e0b130f037cd56d6157d60d25
kqwyf <kqwyfhit@163.com>
2017-06-19 17:46:27 +0800

    Lock

--- ElectricPasswordLock.srcs/sources_1/new/Lock.v
+++ ElectricPasswordLock.srcs/sources_1/new/Lock.v
@@ -24,16 +24,17 @@ module Lock(in,green,red);
     input [7:0] in;
     output green,red;
     reg green,red;
-    wire start,stop,change;
+    wire start,stop,change,reset,changing;
     reg [2:0] p[1:0];
     reg [1:0] step;
     reg [7:0] has;
     reg [5:0] times;
-    reg r_start,r_change;
+    reg r_start,r_change,r_reset;
     initial
     begin
         r_start=0;
         r_change=0;
+        r_reset=0;
         step=0;
         has=8'b00000001;
         times=0;
@@ -45,62 +46,108 @@ module Lock(in,green,red);
         green=0;
     end
     
+    assign changing=r_reset&&r_change;
+    
     //倒计时芯片
     assign start=r_start;
     assign change=r_change;
-    Timeout timer(start,change,stop);
+    assign reset=r_reset;
+    Timeout timer(start,change,reset,stop);
     
     //当输入时开始计时
     always @(posedge in)
+    begin
+        times=times+1;
         r_start=1;
+    end
     
-    //判断输入
+    //判断输入，验证或修改密码
     always @(posedge in[0])
-        if(step==4) 
+        if(changing)
+        begin
+            p[times]=0;
+            times=times+1;
+        end
+        else if(step==4) r_reset=1; //判断是否要修改密码
         else if(has[0]&&p[step]==0) step=step+1;
     always @(posedge in[1])
-        if(has[1]&&p[step]==0) step=step+1;
-    always @(posedge in[2])
-        if(has[2]&&p[step]==0) step=step+1;
-    always @(posedge in[3])
-        if(has[3]&&p[step]==0) step=step+1;
-    always @(posedge in[4])
-        if(has[4]&&p[step]==0) step=step+1;
-    always @(posedge in[5])
-        if(has[5]&&p[step]==0) step=step+1;
-    always @(posedge in[6])
-        if(has[6]&&p[step]==0) step=step+1;
-    always @(posedge in[7])
-        if(has[7]&&p[step]==0) step=step+1;
-    
-    always @(step)
-        if(step==4)
+        if(changing)
         begin
-            green=1;
-            red=0;
+            p[times]=1;
+            times=times+1;
+        end
+        else if(has[1]&&p[step]==0) step=step+1;
+    always @(posedge in[2])
+        if(changing)
+        begin
+            p[times]=2;
+            times=times+1;
+        end
+        else if(has[2]&&p[step]==0) step=step+1;
+    always @(posedge in[3])
+        if(changing)
+        begin
+            p[times]=3;
+            times=times+1;
+        end
+        else if(has[3]&&p[step]==0) step=step+1;
+    always @(posedge in[4])
+        if(changing)
+        begin
+            p[times]=4;
+            times=times+1;
+        end
+        else if(has[4]&&p[step]==0) step=step+1;
+    always @(posedge in[5])
+        if(changing)
+        begin
+            p[times]=5;
+            times=times+1;
+        end
+        else if(has[5]&&p[step]==0) step=step+1;
+    always @(posedge in[6])
+        if(changing)
+        begin
+            p[times]=6;
+            times=times+1;
+        end
+        else if(has[6]&&p[step]==0) step=step+1;
+    always @(posedge in[7])
+        if(changing)
+        begin
+            p[times]=7;
+            times=times+1;
+        end
+        else if(has[7]&&p[step]==0) step=step+1;
+    
+    //判断密码输入是否结束
+    always @(step)
+        if(step==4) r_reset=1;
+    
+    //判断修改密码是否结束
+    always @(times)
+        if(times==4)
+        begin
+            r_start<=0;
+            r_change<=0;
+            r_reset<=0;
+            times<=0;
+            red<=1;
+            green<=0;
         end
     
     //倒计时结束
-    always @(stop)
+    always @(posedge stop)
     begin
-        if(stop==1)
-        begin
-            r_start=0;
-            r_change=0;
-            step=0;
-            has=8'b00000001;
-            times=0;
-            p[0]=0;
-            p[1]=0;
-            p[2]=0;
-            p[3]=0;
-            red=1;
-            green=0;
-        end
+        if(changing);
+        else if(step==4) r_change<=1;
+        else r_change<=0;
+        step<=0;
+        times<=0;
+        r_start<=0;
+        r_reset<=0;
+        if(step==4) red<=0;
+        else red<=1;
+        green<=~red;
     end
-        
-endmodule
-module Timeout(start,change,stop);
-    input start,change;
-    output stop;
 endmodule
